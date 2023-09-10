436800fd7f21d16208fc446f23b4441e640ffc53
AlexanderPasso <alej2498@gmail.com>
2023-09-10 12:50:27 -0500

    Funciones para el monitorea del sensor frontal y doblar a la derecha

--- RobotLaberinto/RobotLaberinto.ino
+++ RobotLaberinto/RobotLaberinto.ino
@@ -1,3 +1,4 @@
+
 #define enB 9
 #define enA 10
 
@@ -7,86 +8,96 @@
 #define rm2 6
 #define offsetEnA 12
 
-#define tCenter A3
-#define eCenter A2
+#define tCenter   A3 // Arduino pin tied to trigger pin on ping sensor.
+#define eCenter   A2 // Arduino pin tied to echo pin on ping sensor.
 #define tRight A5
 #define eRight A4
 #define tLeft A1
 #define eLeft A0
 
-uint8_t maxLimitFront = 9;
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+
 
 uint16_t leftSensor, oldLeftSensor = 0;
 uint16_t rightSensor, oldRightSensor = 0;
 uint16_t centerSensor, oldCenterSensor = 0;
+uint8_t maxLimitFront = 15;       //Limite maximo de distancia del sensor frontal
+bool turnRight = false;   //Doblar derecha
+
+NewPing sonar[3] = {   // Sensor object array.
+  NewPing(tCenter, eCenter, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
+  NewPing(tRight, eRight, MAX_DISTANCE),
+  NewPing(tLeft, eLeft, MAX_DISTANCE)
+};
+
+unsigned int pingSpeed = 50; // How frequently are we going to send out a ping (in milliseconds). 50ms would be 20 times a second.
+unsigned long pingTimer;     // Holds the next ping time.
 
 void setup() {
-  Serial.begin(9600);
-  initPins();
-  delay(1000);
+  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+  pingTimer = millis(); // Start now.
 }
 
 void loop() {
-  readSensors();
-  if (centerSensor < maxLimitFront) {
-    stop();
-  } else {
-    run();
+  // Notice how there's no delays in this sketch to allow you to do other processing in-line while doing distance pings.
+  if (millis() >= pingTimer) {   // pingSpeed milliseconds since last ping, do another ping.
+    pingTimer += pingSpeed;      // Set the next ping time.
+    sonar[0].ping_timer(echoCheck); // Send out the ping, calls "echoCheck" function every 24uS where you can check the ping status.
+  }
+  // Do other stuff here, really. Think of it as multi-tasking.
+
+  if(turnRight){
+    fTurnRight();
   }
 }
 
+void echoCheck() { // Timer2 interrupt calls this function every 24uS where you can check the ping status.
+  // Don't do anything here!
+  if (sonar[0].check_timer()) { // This is how you check to see if the ping was received.
+    // Here's where you can add code.
+    centerSensor = sonar[0].ping_result / US_ROUNDTRIP_CM;
+    Serial.print("Ping: ");
+    Serial.print(centerSensor); // Ping returned, uS result in ping_result, convert to cm with US_ROUNDTRIP_CM.
+    Serial.println("cm");
 
-void speed(int vel) {
-  analogWrite(enB, vel);
-  digitalWrite(lm1, 1);
-  digitalWrite(lm2, 0);
-}
+    if (  (sonar[0].ping_result / US_ROUNDTRIP_CM) < maxLimitFront) {
+      stop();
+      turnRight = true;
+    } else {
+      run();
+    }
 
-void initPins() {
-  for (int i = 4; i <= 10; i++) {
-    pinMode(i, OUTPUT);
   }
-  pinMode(A0, INPUT);
-  pinMode(A1, OUTPUT);
-  pinMode(A2, INPUT);
-  pinMode(A3, OUTPUT);
-  pinMode(A4, INPUT);
-  pinMode(A5, OUTPUT);
+  // Don't do anything here!
 }
 
+//funcion que me permite doblar hacia la derecha
+void fTurnRight(){
+  //Controlar por medio del sensor del centro hasta cuando doblar hacia la derecha
+  while(centerSensor < 12){
+    analogWrite(enA, 200 + offsetEnA);
+    analogWrite(enB, 200);
+    digitalWrite(rm1, 0);
+    digitalWrite(rm2, 1);
+    digitalWrite(lm1, 1);
+    digitalWrite(lm2, 0);
+    delay(300);
+    centerSensor = sonar[0].ping_cm();      //Para medir la distancia actual 
 
-uint16_t readSensor(uint8_t trigger, uint8_t echo) {
-  uint16_t t;  //timepo que demora en llegar el eco
-  uint16_t d;  //distancia en centimetros
+  }
 
-  digitalWrite(trigger, HIGH);
-  delayMicroseconds(10);  //Enviamos un pulso de 10us
-  digitalWrite(trigger, LOW);
+  stop();
+  delay(100);
+  turnRight = false;
 
-  t = pulseIn(echo, HIGH);  //obtenemos el ancho del pulso
-  d = t / 59;               //escalamos el tiempo a una distancia en cm
-  return d;
-}
-
-void readSensors() {
-  uint16_t lSensor = readSensor(tLeft, eLeft);
-  uint16_t rSensor = readSensor(tRight, eRight);
-  uint16_t cSensor = readSensor(tCenter, eCenter);
-
-  leftSensor = (lSensor + oldLeftSensor) / 2;
-  rightSensor = (rSensor + oldRightSensor) / 2;
-  centerSensor = (cSensor + oldCenterSensor) / 2;
-
-
-  oldLeftSensor = leftSensor;
-  oldRightSensor = rightSensor;
-  oldCenterSensor = centerSensor;
+  
 }
 
 
 void run() {
-  analogWrite(enA, 150 + offsetEnA);
-  analogWrite(enB, 150);
+  analogWrite(enA, 200 + offsetEnA);
+  analogWrite(enB, 200);
   digitalWrite(rm1, 1);
   digitalWrite(rm2, 0);
   digitalWrite(lm1, 1);
